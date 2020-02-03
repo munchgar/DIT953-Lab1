c1d46fbd5051bee63aec8789d5d1301b93e4727b
Serecat <60610698+Serecat@users.noreply.github.com>
2020-02-03 16:46:35 +0100

    Add files via upload

---
+++ Saab95Test.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Saab95Test extends Saab95 {
+
+    @Test
+    public void setTurboOn() {
+        assertTrue(turboOn == false);
+
+    }
+
+    @Test
+    public void setTurboOff() {
+        assertTrue(turboOn == false);
+
+    }
+}
