57dbb429a2ba13b5a6eb72d88847bb5d9fba6df1
Bloodwhale <60608239+Bloodwhale@users.noreply.github.com>
2020-02-09 19:50:48 +0100

    Everything BUT the x,y position update during a drive should be correct

--- BigBoy.java
+++ BigBoy.java
@@ -13,10 +13,12 @@ public class BigBoy extends Cars {
      */
     enum RampMode {UP, DOWN}
     RampMode rmp;
-
+    /***
+     * container for the cars
+     */
     ArrayList<Cars> load = new ArrayList<Cars>();
     int loadSize;
-
+    BigBoy bigboy = new BigBoy();
     /***
      * Constructor for BigBoy (a truck).
      */
@@ -40,10 +42,55 @@ public class BigBoy extends Cars {
         if (rmp != RampMode.DOWN) throw new IllegalStateException("The ramp must be down to load a car");
         if(getCurrentSpeed() != 0) throw new IllegalStateException("The BigBoy is moving");
         if(loadSize + car.size > 10) throw new IllegalStateException("There is not enough room on the BigBoy to load the car");
-
         load.add(car);
         loadSize += car.size;
+        changeXy(bigboy, car);
+        }
+
+
+    public void changeXy(BigBoy bigboy, Cars car) {
+        for (int i = 0; i < load.size()+1; i++){
+            car.x = bigboy.x;
+            car.y = bigboy.y;
+        }
     }
+
+    /***
+     * Unloads the car that was last loaded onto the truck
+     */
+    public void unload() {
+        if (getCurrentSpeed() != 0) throw new IllegalStateException("The BigBoy is moving");
+        if (rmp!= RampMode.DOWN) throw new IllegalStateException("Unable to unload cars as the ramp is not down");
+        int indexCar = load.size() - 1;
+        Cars car = load.remove(indexCar);
+        load.remove(indexCar);
+        car.x = car.x + 2;
+        car.y = car.y + 2;
+    }
+    @Override
+
+    public void move() {
+
+        switch (dir) {
+            case NORTH:
+                y += getCurrentSpeed();
+                break;
+            case EAST:
+                x += getCurrentSpeed();
+                break;
+            case SOUTH:
+                y -= getCurrentSpeed();
+                break;
+            case WEST:
+                x -= getCurrentSpeed();
+                break;
+        }
+        for (int index = 0; index < load.size()+1; index++) {
+            Cars car = load.get(index);
+            car.x = bigboy.x;
+        }
+    }
+
 
 
 }
