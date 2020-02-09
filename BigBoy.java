49d7490811e73274c247dbdc5ff8e4c92d28222a
Bloodwhale <60608239+Bloodwhale@users.noreply.github.com>
2020-02-09 20:14:55 +0100

    Added comments and changed the class name from Cars to Car

--- BigBoy.java
+++ BigBoy.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * Class for a truck.
  */
 
-public class BigBoy extends Cars {
+public class BigBoy extends Car {
 
     /***
      * enumeration for the states of the ramp, namely UP or DOWN.
@@ -16,7 +16,7 @@ public class BigBoy extends Cars {
     /***
      * container for the cars
      */
-    ArrayList<Cars> load = new ArrayList<Cars>();
+    ArrayList<Car> load = new ArrayList<Car>();
     int loadSize;
     BigBoy bigboy = new BigBoy();
     /***
@@ -36,7 +36,7 @@ public class BigBoy extends Cars {
      * Method that loads a truck with cars, with a various amount of conditions that need to be met.
      * @param car <-- takes any car that is not a BigBoy as parameter.
      */
-    public void load(Cars car) {
+    public void load(Car car) {
         if (!checkDistance(car, this)) throw new IllegalStateException("The vehicles are too far apart or too close");
         if(car instanceof BigBoy) throw new IllegalStateException("You can't load a BigBoy on another BigBoy");
         if (rmp != RampMode.DOWN) throw new IllegalStateException("The ramp must be down to load a car");
@@ -47,26 +47,33 @@ public class BigBoy extends Cars {
         changeXy(car);
         }
 
-
-    public void changeXy(Cars car) {
+    /***
+     * Small helper function for changing the x and y value of a car to the x and y of a truck.
+     * @param car <-- takes any car as a parameter.
+     */
+    public void changeXy(Car car) {
             car.x = this.x;
             car.y = this.y;
     }
 
     /***
      * Unloads the car that was last loaded onto the truck
+     * Also changes the x and y value of a car so that the car is not too close to the truck.
      */
     public void unload() {
         if (getCurrentSpeed() != 0) throw new IllegalStateException("The BigBoy is moving");
         if (rmp!= RampMode.DOWN) throw new IllegalStateException("Unable to unload cars as the ramp is not down");
         int indexCar = load.size() - 1;
-        Cars car = load.remove(indexCar);
+        Car car = load.remove(indexCar);
         load.remove(indexCar);
         car.x = car.x + 2;
         car.y = car.y + 2;
     }
     @Override
-
+    /***
+     * Overriden move function that works the same way but also loops through the list of cars (load)
+     * and changes the specific cars X and Y to the X and Y of the truck.
+     */
     public void move() {
 
         switch (dir) {
@@ -84,7 +91,7 @@ public class BigBoy extends Cars {
                 break;
         }
         for (int index = 0; index < load.size() -1; index++) {
-            Cars car = load.get(index);
+            Car car = load.get(index);
             car.x = this.x;
             car.y = this.y;
         }
