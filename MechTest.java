70a2cd941f8944cb0fe6533b07c3bd43e6e29590
Serecat <60610698+Serecat@users.noreply.github.com>
2020-02-10 21:56:48 +0100

    uppdaterade testen

--- MechTest.java
+++ MechTest.java
@@ -6,17 +6,20 @@ import static org.junit.Assert.*;
 
 public class MechTest <T extends Cars> {
 ArrayList<Volvo240> volvoGarage;
-Volvo240 volvo = new Volvo240();
+Volvo240 volvo1 = new Volvo240();
 int load = 0;
 
     @Test
     public void load() {
-        volvoGarage.add(volvo);
+        volvoGarage.add(volvo1);
         assertTrue(load == 1);
 
     }
 
     @Test
     public void unoload() {
+        Volvo240 volvo1 = volvoGarage.remove(volvoGarage.size() - 1);
+        assertTrue(load == 0);
+
     }
 }
