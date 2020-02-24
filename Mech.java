97b0e37f0620eb06e2af85047082bd2647df0b0b
munchgar <57719594+munchgar@users.noreply.github.com>
2020-02-24 08:18:51 +0100

    improved version

--- Mech.java
+++ Mech.java
@@ -1,16 +1,19 @@
 import java.util.ArrayList;
 
-/***
+/**
  * The class of a garage that stores cars based on the specific car type
  * @param <T>
+ * 
+ * @author Jonathan Héden,
+ * @author Nicklas Strandevall,
+ * @author Kevin Rylander
  */
-public class Mech <T extends Car> {
+public class Mech <T extends Car> implements Loadable<T> {
 
     private final int maxLoad;
-    private int load;
     private ArrayList<T> mechGarage;
 
-    /***
+    /**
      * Gives the garage a car limit and a list of the cars that are stored
      * @param maxLoad
      */
@@ -19,24 +22,24 @@ public class Mech <T extends Car> {
         mechGarage = new ArrayList<T>();
     }
 
-    /***
+    /**
      * Stores a car in the garage
      * @param car of the specific type cartype
      */
     public void load(T car) {
-        if(load + 1 > maxLoad) return;
+        if(mechGarage.size() >= maxLoad) throw new IllegalArgumentException("Garage is full");
 
         mechGarage.add(car);
-        load++;
     }
 
-    /***
+    /**
      * Removes a car from the garage
      * @return returns the car that was removed
      */
-    public T unoload() {
+    public T unload() {
+        if(mechGarage.size() <= 0) throw new IllegalArgumentException("Garage is empty");
+
         T car = mechGarage.remove(mechGarage.size() - 1);
-        load--;
         return car;
     }
 }
