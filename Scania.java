9f405a3f6a4121869d5cd0d8016e5e6c810fad72
munchgar <57719594+munchgar@users.noreply.github.com>
2020-02-28 16:33:55 +0100

    Great version

--- Scania.java
+++ Scania.java
@@ -10,7 +10,7 @@ import java.awt.*;
 public class Scania extends Car {
 
     private int degree = 0;
-    private boolean floatBedUp = false;
+    private boolean floatBedDown = true;
 
     public static final int MAX_TILT = 70;
     public static final int MIN_TILT = 0;
@@ -30,8 +30,8 @@ public class Scania extends Car {
         return degree;
     }
 
-    public boolean floatBedUp() {
-        return floatBedUp;
+    public boolean floatBedDown() {
+        return floatBedDown;
     }
 
     /**
@@ -40,15 +40,16 @@ public class Scania extends Car {
      * @param deg
      */
     public void tip(int deg) {
-        if (getCurrentSpeed() != 0 ) throw new IllegalStateException();
-        if(deg <= 0) throw new IllegalArgumentException();
+        if (getCurrentSpeed() != 0) throw new IllegalStateException();
+        if(deg < 0 || deg > 70) throw new IllegalArgumentException();
 
         if (degree + deg > MAX_TILT)
             degree = MAX_TILT;
         else
             degree += deg;
 
-        floatBedUp = true;
+        if(degree > MIN_TILT)
+            floatBedDown = false;
     }
 
     /**
@@ -58,7 +59,7 @@ public class Scania extends Car {
      */
     public void sink(int deg) {
         if (getCurrentSpeed() != 0 ) throw new IllegalStateException();
-        if(deg <= 0) throw new IllegalArgumentException();
+        if(deg < 0) throw new IllegalArgumentException();
 
         if (degree - deg < MIN_TILT)
             degree = MIN_TILT;
@@ -66,31 +67,17 @@ public class Scania extends Car {
             degree -= deg;
 
         if(degree == MIN_TILT)
-            floatBedUp = false;
+            floatBedDown = true;
     }
 
-    /**
-     * Overriden gas function that also says that if the degree of the flatbed is higher than 0
-     * it will give an error, preventing the truck from driving with a flatbed that has tipped.
-     * 
-     * @param amount - takes this as argument. (value between 0 and 1 or error).
-     */
-    // @Override
-    // public void gas(double amount){
-    //     if (degree > 0) throw new IllegalStateException("The flatbed is still up");
-    //     if (amount < 0 || amount > 1) throw new IllegalArgumentException();
-
-    //     incrementSpeed(amount);
-    // }
-
     protected void incrementSpeed(double amount){
-        if(!floatBedUp())
-            setCurrentSpeed(getCurrentSpeed() + amount);
+        if(floatBedDown())
+            setCurrentSpeed(Math.min(getCurrentSpeed() + amount, getEnginePower()));
     }
 
     protected void decrementSpeed(double amount){
-        if(!floatBedUp())
-            setCurrentSpeed(getCurrentSpeed() - amount);
+        if(floatBedDown())
+            setCurrentSpeed(Math.max(getCurrentSpeed() - amount, 0));
     }
 }
 
