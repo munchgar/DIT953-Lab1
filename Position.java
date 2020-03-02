1bcd89c67e70607d3491566162fc4719af93aa09
munchgar <57719594+munchgar@users.noreply.github.com>
2020-03-02 15:55:40 +0100

    Divine version

--- Position.java
+++ Position.java
@@ -1,10 +1,26 @@
 public class Position {
 
-    double x = 0; // X-coordinate for car
-    double y = 0; // Y-coordinate for car
+    private double x = 0; // X-coordinate for car
+    private double y = 0; // Y-coordinate for car
 
     public Position(double x, double y) {
         this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+    
+    public double getY() {
+        return y;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+    
+    public void setY(double y) {
         this.y = y;
     }
 }
