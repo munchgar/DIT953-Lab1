0a1356801b78491b04726a05bb349e955ce5c115
munchgar <57719594+munchgar@users.noreply.github.com>
2020-03-02 15:56:53 +0100

    Divine version

--- Main.java
+++ Main.java
@@ -7,31 +7,16 @@ public class Main {
 
     public static void main(String[] args) {
         CarModel model = new CarModel();
+        CarFactory factory = new CarFactory();
 
-        model.addCar(new Volvo240());
-        model.addCar(new Saab95());
-        model.addCar(new Scania());
-        model.addCar(new Saab95());
-        model.addCar(new Volvo240());
+        model.addCar(factory.createVolvo240());
+        model.addCar(factory.createSaab95());
+        model.addCar(factory.createScania());
 
-        CarView view = new CarView("CarSim 2.0", model);
         CarController controller = new CarController(model);
+        CarView view = new CarView("CarSim 2.0");
+        model.subscribe(view.getDrawPanel());
 
         model.startModel();
-
-        // CarModel model1 = new CarModel();
-
-        // model1.addCar(new Volvo240());
-        // model1.addCar(new Saab95());
-        // model1.addCar(new Scania());
-        // model1.addCar(new Saab95());
-        // model1.addCar(new Volvo240());
-        // model1.addCar(new Saab95());
-        // model1.addCar(new Scania());
-        // model1.addCar(new Scania());
-
-        // CarView view1 = new CarView("CarSim 2.0", model1);
-
-        // model1.startModel();
     }
 }
